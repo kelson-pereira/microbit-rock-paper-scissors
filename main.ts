2e14babdc433e5a0269afc0aeaa7ad7995c4f959
Kelson Pereira <kelson.op@gmail.com>
2021-02-28 14:19:53 -0300

    added counter when button b pressed/ added total counter where a+b buttons pressed/ added sound effects

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
-let hand = 0
 input.onButtonPressed(Button.A, function () {
     play()
 })
 function play () {
     hand = randint(1, 3)
+    music.playTone(440, music.beat(BeatFraction.Half))
     if (hand == 1) {
         basic.showLeds(`
             . . . . .
@@ -29,8 +29,40 @@ function play () {
             # # . . #
             `)
     }
-    music.playTone(440, music.beat(BeatFraction.Half))
 }
+input.onButtonPressed(Button.AB, function () {
+    music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.OnceInBackground)
+    basic.showNumber(points)
+    basic.pause(2000)
+    basic.clearScreen()
+    start()
+})
+input.onButtonPressed(Button.B, function () {
+    points = points + 1
+    music.playTone(523, music.beat(BeatFraction.Quarter))
+    basic.showNumber(points)
+    basic.pause(200)
+    basic.clearScreen()
+})
 input.onGesture(Gesture.Shake, function () {
     play()
 })
+function start () {
+    music.startMelody(music.builtInMelody(Melodies.Funk), MelodyOptions.OnceInBackground)
+    points = 0
+    basic.showString("PLAY")
+    basic.showIcon(IconNames.Happy)
+    basic.showLeds(`
+        . . . . .
+        . # . . .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(200)
+    basic.clearScreen()
+}
+let points = 0
+let hand = 0
+start()
